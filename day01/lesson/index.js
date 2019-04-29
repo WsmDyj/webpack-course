d62d6dd36d01ce185ed34e9f579505d8a4c5fc41
邬嗣敏 <wusimin7@ZBMAC-VM7REHTD5.local>
2019-04-29 15:14:49 +0800

    第一课--webpack基本概念

---
+++ day01/lesson/index.js
@@ -0,0 +1,20 @@
+// ES moudule 模块引入方式，浏览器无法识别。借助webpack翻译一下
+// commonJS 模块引入规范 Node
+// CMD
+// AMD
+
+// webpack 模块打包工具
+
+// import Header from './header.js'
+// import Sidebar from './sidebar.js'
+// import Content from './content.js'
+
+let Header = require('./header.js')
+let Sidebar = require('./sidebar.js')
+let Content = require('./content.js')
+
+new Header()
+new Sidebar()
+new Content()
+
+
